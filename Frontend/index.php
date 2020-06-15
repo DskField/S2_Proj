e0311b7bb6e582800e57a9b0bab16aaac1b7f65a
Davi <davidkvelderman@gmail.com>
2020-06-15 11:36:10 +0200

    Footer, index, module header added

--- Frontend/index.php
+++ Frontend/index.php
@@ -1,1 +1,32 @@
+<!DOCTYPE html>
+
 <?php
+include "Includes/moduleheader.php"
+?>
+
+<html lang="nl">
+
+    <head>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <title>
+            Uitwerkingen
+        </title>
+    </head>
+
+    <body>
+
+        <?php
+            include "Includes/header.php";
+        ?>
+
+        <main>
+
+        </main>
+
+        <?php
+            include "Includes/footer.php"
+        ?>
+
+    </body>
+
+</html>
