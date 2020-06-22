19a08888047db0bba91f05717aa6038e1222e96e
Davi <davidkvelderman@gmail.com>
2020-06-22 22:42:30 +0200

    Bugfix with footer overlap

--- Frontend/Includes/meta.php
+++ Frontend/Includes/meta.php
@@ -5,7 +5,6 @@
 
     <link href="/S2_Proj/Frontend/CSS/style.css" type="text/css" rel="stylesheet">
     <link href="/S2_Proj/Frontend/CSS/dropdown.css" type="text/css" rel="stylesheet">
-    <link href="/S2_Proj/Frontend/CSS/contact.css" type="text/css" rel="stylesheet">
 
     <script src="/S2_Proj/Frontend/Scripts/jquery-3.4.1.js"></script>
     <script src="/S2_Proj/Frontend/Scripts/script.js" defer></script>
--- Frontend/contact.php
+++ Frontend/contact.php
@@ -6,9 +6,23 @@ include "Includes/moduleheader.php"
 
 <html lang="nl">
 
-    <?php
-        require "Includes/meta.php";
-    ?>
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="Hoofdpagina">
+    <meta name="keywords" content="keyword 1, keyword 2">
+
+    <link href="/S2_Proj/Frontend/CSS/style.css" type="text/css" rel="stylesheet">
+    <link href="/S2_Proj/Frontend/CSS/dropdown.css" type="text/css" rel="stylesheet">
+    <link href="/S2_Proj/Frontend/CSS/contact.css" type="text/css" rel="stylesheet">
+
+    <script src="/S2_Proj/Frontend/Scripts/jquery-3.4.1.js"></script>
+    <script src="/S2_Proj/Frontend/Scripts/script.js" defer></script>
+    <script src="/S2_Proj/Frontend/Scripts/dropdown.js" defer></script>
+
+    <title>
+        Mojo Concerts
+    </title>
+</head>
 
     <body>
 
