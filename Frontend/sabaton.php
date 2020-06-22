1e07bd597e03f79ca445ff64d7ff748f5c70fe93
Davi <davidkvelderman@gmail.com>
2020-06-22 11:29:01 +0200

    Meta moved to separate PHP file

---
+++ Frontend/sabaton.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+
+<?php
+include "Includes/moduleheader.php"
+?>
+
+<html lang="nl">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="Hoofdpagina">
+    <meta name="keywords" content="keyword 1, keyword 2">
+
+    <link href="CSS/style.css" type="text/css" rel="stylesheet">
+    <link href="CSS/dropdown.css" type="text/css" rel="stylesheet">
+
+    <script src="Scripts/jquery-3.4.1.js"></script>
+    <script src="Scripts/script.js" defer></script>
+    <script src="Scripts/dropdown.js" defer></script>
+
+    <title>
+        Mojo Concerts
+    </title>
+</head>
+
+<body>
+
+<?php
+include "Includes/header.php";
+?>
+
+<main>
+
+    <img class="bigImg" src="Images/Sabaton.jpg" alt="sabaton band">
+
+    <section>
+        <h1>Kop</h1>
+        <hr>
+        <img class="textimageleft" src="Images/20200212-COPENHAGEN-00177.jpg" alt="sabaton copenhagen">
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lobortis diam id ornare suscipit. Mauris sit amet quam massa. Maecenas sodales nibh nunc, id semper lorem viverra sed. Ut convallis, tortor at placerat rutrum, felis erat pellentesque elit, et fermentum eros lacus id nulla. Quisque vitae arcu augue. Cras sapien neque, finibus ac neque dictum, convallis consectetur magna. Vivamus vitae lacus tristique, dignissim enim nec, mollis tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin vitae nisi nisl. Donec pulvinar viverra mollis. Nam eu mauris gravida, placerat magna eu, lobortis tortor.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lobortis diam id ornare suscipit. Mauris sit amet quam massa. Maecenas sodales nibh nunc, id semper lorem viverra sed. Ut convallis, tortor at placerat rutrum, felis erat pellentesque elit, et fermentum eros lacus id nulla. Quisque vitae arcu augue. Cras sapien neque, finibus ac neque dictum, convallis consectetur magna. Vivamus vitae lacus tristique, dignissim enim nec, mollis tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin vitae nisi nisl. Donec pulvinar viverra mollis. Nam eu mauris gravida, placerat magna eu, lobortis tortor.
+        </p>
+
+    </section>
+
+
+    <section>
+        <hr>
+        <img class="textimageright" src="Images/20200212-COPENHAGEN-00177.jpg" alt="sabaton copenhagen">
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lobortis diam id ornare suscipit. Mauris sit amet quam massa. Maecenas sodales nibh nunc, id semper lorem viverra sed. Ut convallis, tortor at placerat rutrum, felis erat pellentesque elit, et fermentum eros lacus id nulla. Quisque vitae arcu augue. Cras sapien neque, finibus ac neque dictum, convallis consectetur magna. Vivamus vitae lacus tristique, dignissim enim nec, mollis tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin vitae nisi nisl. Donec pulvinar viverra mollis. Nam eu mauris gravida, placerat magna eu, lobortis tortor.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lobortis diam id ornare suscipit. Mauris sit amet quam massa. Maecenas sodales nibh nunc, id semper lorem viverra sed. Ut convallis, tortor at placerat rutrum, felis erat pellentesque elit, et fermentum eros lacus id nulla. Quisque vitae arcu augue. Cras sapien neque, finibus ac neque dictum, convallis consectetur magna. Vivamus vitae lacus tristique, dignissim enim nec, mollis tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin vitae nisi nisl. Donec pulvinar viverra mollis. Nam eu mauris gravida, placerat magna eu, lobortis tortor.
+        </p>
+    </section>
+
+</main>
+
+<?php
+include "Includes/footer.php"
+?>
+
+</body>
+
+</html>
